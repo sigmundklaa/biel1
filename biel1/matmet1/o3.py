801de7c67d16436f77b63227ed74945ee08001ef
Sigmund Klåpbakken <sigmundklaa@outlook.com>
2022-10-05 18:06:59 +0200

    gamertime

--- biel1/matmet1/o3.py
+++ biel1/matmet1/o3.py
@@ -1,5 +1,8 @@
 
+import functools
 import numpy as np
+
+NUM_DEC = 5
 
 def f(x):
     return np.exp(x) + x - 3
@@ -7,12 +10,24 @@ def f(x):
 def df(x):
     return np.exp(x) + 1
 
-def newton(r):
+def newton(r, *_):
     return r - (f(r)/df(r))
 
-arr = [1] * 10
+def reduced():
+    return round(functools.reduce(newton, range(10), 1), NUM_DEC)
 
-for i in range(1, len(arr)):
-    arr[i] = newton(arr[i-1])
+def AAAAAAAAAAAAAAAAAAH():
+    return round(functools.reduce(
+        lambda x, _: x - ((np.exp(x) + x - 3)/(np.exp(x) + 1)),
+        range(10), 1),
+        NUM_DEC)
 
-print(arr[-1])
+def cached():
+    arr = [1] * 10
+
+    for i in range(1, len(arr)):
+        arr[i] = newton(arr[i-1])
+
+    return round(arr[-1], NUM_DEC)
+
+print(reduced(), cached(), AAAAAAAAAAAAAAAAAAH())
