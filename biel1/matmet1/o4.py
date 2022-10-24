961363570ec04543b23843931d496164bf4ff49c
sigjig <sigmundklaa@outlook.com>
2022-10-24 22:02:34 +0200

    i hate this :D

--- biel1/matmet1/o4.py
+++ biel1/matmet1/o4.py
@@ -10,13 +10,13 @@ matplotlib.use('TkAgg')
 
 def o_1():
     def f(x):
-        return np.exp(1-x**2)
+        return 2 * np.exp(1-x**2)
 
     def df(x):
         return -2 * x * f(x)
 
     def tang(x):
-        return -2 * (x - 1) + f(1)
+        return df(1) * (x - 1) + f(1)
 
     xvals = np.arange(-2, 2, 0.1)
 
@@ -25,23 +25,37 @@ def o_1():
     pyplot.show()
 
     print(f(1) + df(1) * 0.1)
+    print(f(1.1))
 
 def o_2():
     def f(x):
         return x ** 2 * (np.sin(x) * np.cos(x) + 1)
 
-    xvals = np.arange(-10, 10, 0.1)
+    xvals = np.arange(-3, 3, 0.1)
     pyplot.plot(xvals, f(xvals))
     pyplot.show()
 
+def o_3():
+    def f(x):
+        return ((x ** 2) * np.sin(x)) + (2 * x * np.cos(x)) + (2 * np.sin(x)) + ((1/3) * (x ** 3))
+
+    print(f(3) - f(-3))
+
 def o_5():
     def integ(x):
-        return (1/(math.cos(x)*math.sin(x)) + x)
+        return (-1/(math.cos(x)) + x)
 
-    return (integ(np.pi/4) - integ(0))
+    print(integ(np.pi/4) - integ(0))
 
 def o_7():
-    pass
+    def f(x):
+        return np.sqrt(x + x**3)
+
+    def rkm(f, start, stop, n):
+        dx = (stop - start) / n
+        return sum(f(x + dx/2)*dx for x in np.arange(start, stop, dx))
+
+    print(round(rkm(f, 0, 1, 10), 6))
 
 def o_8():
     def f(x):
@@ -61,8 +75,8 @@ def o_8():
         )
 
     #print(np.arange(0, 1, 0.1))
-    print(simpson(0, 1, 10))
+    print(round(simpson(0, 1, 10), 8))
 
 if __name__ == '__main__':
-    o_2()
+    o_8()
 
